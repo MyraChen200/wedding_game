e5412873659cb98fcf7ed54c2ed0051b0bbf478c
MyraChen <myra@thetigerparty.com>
2019-04-08 18:43:27 +0800

    Commit forntend app and npm init

--- wedding/server/models.py
+++ wedding/server/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Camapign(models.Model):
+class Campaign(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     start_date = models.DateTimeField(auto_now=False)
     
