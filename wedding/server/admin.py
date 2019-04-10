adb5d59ce4a668e15df16e59b753e90e77cc0004
MyraChen <myra@thetigerparty.com>
2019-04-10 23:52:24 +0800

    Add Campaign and Games model/fixtures

--- wedding/server/admin.py
+++ wedding/server/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from server.models import Campaign
+from server.models import Campaign, Game, Campaign_Game
 
 admin.site.register(Campaign)
+admin.site.register(Game)
+admin.site.register(Campaign_Game)
--- wedding/server/models.py
+++ wedding/server/models.py
@@ -4,4 +4,21 @@ from django.db import models
 class Campaign(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     start_date = models.DateTimeField(auto_now=False)
+    # game = models.ManyToManyField('Game', through='Campaign_Game')
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    campaign = models.ManyToManyField('Campaign', through='Campaign_Game')
+
+    def __str__(self):
+        return self.name
+
+class Campaign_Game(models.Model):
+    campaign = models.ForeignKey('Campaign', on_delete=models.CASCADE)
+    game = models.ForeignKey('Game', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}_{}".format(self.campaign.name, self.game.name)
     
