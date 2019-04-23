bac9b1b40243883874892bbcba36173a0475e366
MyraChen <myra@thetigerparty.com>
2019-04-24 00:20:26 +0800

    Add photos model

--- wedding/server/models.py
+++ wedding/server/models.py
@@ -18,7 +18,12 @@ class Game(models.Model):
 class Campaign_Game(models.Model):
     campaign = models.ForeignKey('Campaign', on_delete=models.CASCADE)
     game = models.ForeignKey('Game', on_delete=models.CASCADE)
+    photo_frame_path = models.FileField(upload_to='photo_frame/')
 
     def __str__(self):
         return "{}_{}".format(self.campaign.name, self.game.name)
-    
+
+class Photos(models.Model):
+    user = models.CharField(max_length=255, null=False, blank=False)
+    upload_path = models.CharField(max_length=255, null=False, blank=False)
+    show_path = models.CharField(max_length=255, null=False, blank=False)
